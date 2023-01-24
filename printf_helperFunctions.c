2b233c0a0328f476c31a2dee532771432fa7b68c
EzirimDaniel <Ezirimdaniel080@gmail.com>
2023-01-24 21:46:02 +0100

    Update printf_helperFunctions.c

--- printf_helperFunctions.c
+++ printf_helperFunctions.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * rev_string - reverses a string in place
+ * rev_string - this function reverses a string in place
  *
  * @s: string to reverse
  * Return: A pointer to a character
